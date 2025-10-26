87761716253861b1a716c252aab17120bfa18983
Power366 <172065612+Power366@users.noreply.github.com>
2025-10-26 18:50:52 -0500

    feat: fetch card img and render it

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import '../styles/App.css'
+import Card from './Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>dasdasdas</h1>
+    <Card url="65D9lmbH8Nn8KKQRqR"></Card>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,56 @@
+import { get } from "http";
+import { useEffect, useState } from "react";
+import "../styles/Card.css";
+export default function Card({url}){
+    const [wowObj,setWowObj]=useState("");
+    const urlKey = url;
+    const fetchUrl = `https://api.giphy.com/v1/gifs/${urlKey}?api_key=hiwceFnwYRL9041M3QiLIACGRJccvlNO&rating=g`
+/*
+    async function loadJson(url) {
+        let result = await fetch(url)
+
+        if (result.status == 200) {
+            let json = await result.json();
+            return json
+        } else {
+            throw new Error(result.status);
+        }
+        
+    }
+    async function getData(uri) {
+  try {
+    const response = await fetch(uri);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}*/
+
+useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch(fetchUrl)
+        .then(response => response.json())
+        .then(data => setWowObj(data));
+
+// empty dependency array means this effect will only run once (like componentDidMount in classes)
+}, []);
+
+    return (
+        <div className="block">
+            {
+        wowObj!="" &&<img src={wowObj.data.images.original.url} alt="warlock" />}
+        <p>
+            {
+            wowObj!=""&& wowObj.data.title
+            }
+        </p>
+
+
+        </div>
+    )
+}
